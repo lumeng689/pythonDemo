8f2de547fa370cb3167853ba727c7a05db8ef099
lumeng <lumeng689@163.com>
2014-12-04 20:06:19 +0800

    add test

--- org/luapp/py/code/Snippet001.py
+++ org/luapp/py/code/Snippet001.py
@@ -1,3 +1,45 @@
 __author__ = 'lumeng'
 
-print 'hello world'
+myString = "hello world"
+
+print myString
+
+print "%s is number %d!" % ("Python",1)
+
+import sys
+print >> sys.stderr, 'Fatal error!'
+
+# user = raw_input("Enter your name:")
+# print user
+
+print int("123")
+
+print 3//2
+print 3**2
+
+print 3 < 4 and 2 > 1
+
+import decimal
+
+print 1.1
+print decimal.Decimal('1.1')
+
+pystr = 'python'
+iscool = 'iscool'
+
+print pystr[0]
+
+print pystr[-5:-1]
+
+print pystr * 2
+
+aList = [1,2,3,4,5]
+print len(aList)
+
+aDict = {"host":"earth"}
+aDict['port'] = 80
+
+print aDict
+
+for key in aDict:
+    print key , aDict[key]
