364680bd67f7256aea74e14ac05fb0ec60bae305
lumeng <lumeng689@163.com>
2014-12-05 18:58:08 +0800

    1

--- org/luapp/py/code/Snippet001.py
+++ org/luapp/py/code/Snippet001.py
@@ -1,3 +1,10 @@
+#/usr/bin/env python
+
+"this is a test module"
+
+import sys
+import os
+
 __author__ = 'lumeng'
 
 myString = "hello world"
@@ -6,8 +13,12 @@ print myString
 
 print "%s is number %d!" % ("Python",1)
 
-import sys
+
 print >> sys.stderr, 'Fatal error!'
+print sys.platform
+print sys.version
+
+print os.linesep
 
 # user = raw_input("Enter your name:")
 # print user
@@ -43,3 +54,91 @@ print aDict
 
 for key in aDict:
     print key , aDict[key]
+
+x = 3
+if x < .0:
+    print "x must be greater than 0"
+elif x < 1:
+    print "x < 1"
+else:
+    print "xxxxx"
+
+counter = 0
+while counter < 5:
+    print "loop :%d" % counter
+    counter += 1
+
+for item in ['a','b','c','d']:
+    print item
+
+for item in ['a','b','c','d']:
+    print item,
+
+foo = 'abcdefg'
+
+for i,ch in enumerate(foo):
+    print ch,'%d' % i
+
+squared = [x**2 for x in range(4)]
+
+for i in squared:
+    print i,
+
+
+squared = [x**2 for x in range(16) if not x % 2]
+
+for i in squared:
+    print i,
+
+fobj = open('d:/test/mdm.sql', 'r')
+
+# for eachline in fobj:
+    # print eachline
+
+fobj.close()
+
+def addMe2Me(x):
+    print 'apply + operation to argument'
+    return (x + x)
+
+print addMe2Me(3)
+
+
+class FooClass(object):
+    """my very first class"""
+    version = 0.1
+
+    def __init__(self, nm='John Doe'):
+        self.name = nm
+        print 'Create a class instance'
+
+    def showName(self):
+        print 'Your name is',self.name
+        print 'My name is ',self.__class__.__name__
+
+    def showVersion(self):
+        print self.version
+
+
+foo1 = FooClass()
+foo1.showName()
+foo1.showVersion()
+
+
+# if import __name__ is module name,if run,__name__ is '__main__'
+print __name__
+
+print type(12)
+
+a = 'ab'
+b = a
+print a is b
+
+b = 'a' + 'b'
+print a is b
+
+s='abcdefg'
+
+#inverse
+print s[::-1]
+
